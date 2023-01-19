ab7f17194d69ea810ea369c3d0d5a8157051ad96
Ajaz7267 <ajaz726@gmail.com>
2023-01-19 12:20:00 +0530

    WelcomeToLineComparision

---
+++ LineComparsion.java
@@ -0,0 +1,20 @@
+//STEP 1
+//Start with Displaying Welcome to Line Comparison Computation Program on Master Branch
+//STEP 2
+//As a fan of geometry, I want to model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system,So that I can calculate its length
+//A Length as 2 Points (x1, y1) and (x2, y2)
+// - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)
+//STEP 3
+//As a fan of geometry, I want to check equality of two lines based on the end points, So that I know when two lines are the equal. - Using Java equals method
+//- Using Java equals method to check equality of 2 Lengths is preferable.
+//STEP 4
+//As a fan of geometry, I want to compare two lines based on the end points, So that I know one line is equal, greater or less than the other line.
+//    - Using Java compareTo method to compare 2 Lengths is preferable.
+package LineComparsionProblem;
+
+public class LineComparsion {
+    public static void main(String args[]){
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+}
